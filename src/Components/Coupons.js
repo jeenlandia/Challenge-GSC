502e686a8d13285513b37eff552ba0bf6cd8128a
Your Name <you@example.com>
2021-11-11 18:51:45 -0300

    View changes

---
+++ src/Components/Coupons.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../Styles/Coupons.css';
+
+function Coupons() {
+    return (
+        <div className="cop">
+            <div className="item">1</div>
+            <div className="item">2</div>
+            <div className="bottom-item item">3</div>    
+        </div>
+    )
+}
+
+export default Coupons;
