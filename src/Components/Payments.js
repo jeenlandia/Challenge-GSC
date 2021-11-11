77f7ea36b6ea57d283a7ad6939f22534805952c5
Your Name <you@example.com>
2021-11-11 18:52:22 -0300

    Adding products

--- src/Components/Payments.js
+++ src/Components/Payments.js
@@ -1,19 +1,18 @@
-import React from "react";
-import '../Styles/Payments.css';
+import React from 'react'
+import '../Styles/Payments.css'
 
-function Payments(){
-    return(
-       <div className="grid">
-           <div class="item-1">1</div>
-           <div class="item-2">2</div>  
-           <div class="item-3">3</div>  
-           <div class="item-4">4</div> 
-           <div class="item-5">5</div>
-           <div class="item-6">6</div>
-           <div class="item-7">7</div>
-           <div class="item-8">volver</div>   
-       </div>
+function Payments() {
+    return (
+        <div className="pay">
+           <div className="item-pay">$</div>
+           <div className="item-pay">VISA</div>  
+           <div className="item-pay">MASTER</div>  
+           <div className="item-pay">UBER</div> 
+           <div className="item-pay">DEBITO</div>
+           <div className="item-pay">+</div>
+           <div className="item-payout item-pay">PAGAR</div>
+        </div>
     )
-};
+}
 
 export default Payments;
