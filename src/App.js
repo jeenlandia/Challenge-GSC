0991ea4309c3c2ecccf57fff9fa00d2f371569f9
Your Name <you@example.com>
2021-11-11 18:49:52 -0300

    Adding components in App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import './App.css'
 import NavBar from './Components/NavBar';
 import Drawer from './Components/Drawer';
 import Payments from './Components/Payments';
+import Coupons from './Components/Coupons';
+import Products from './Components/Products';
+import Table from './Components/Table';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Drawer/>
-      <Payments/>
+      <div className="grid-app">
+        <div><Table/></div>
+        <div><Products/></div>
+        <div><Coupons/></div>
+        <div><Payments/></div>
+      </div>
     </div>
   );
 }
