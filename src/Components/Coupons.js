e3bc468a0d07ffea8e6ed236deeb817d266401e8
Jen Regalado <80419685+jeenlandia@users.noreply.github.com>
2021-11-12 01:20:38 -0300

    finish compont

--- src/Components/Coupons.js
+++ src/Components/Coupons.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import '../Styles/Coupons.css';
+import React from "react";
+import "../Styles/Coupons.css";
+import empleImage from "../images/empleados.jpeg";
+import clientImage from "../images/view.jpeg";
+import discountImage from "../images/descuento.jpeg";
 
 function Coupons() {
-    return (
-        <div className="cop">
-            <div className="item">1</div>
-            <div className="item">2</div>
-            <div className="bottom-item item">3</div>    
-        </div>
-    )
+  return (
+    <div className="cop">
+      <div className="item-cop">
+        <img src={empleImage} />
+        <h4>DESCUENTO EMPLEADOS </h4>
+      </div>
+      <div className="item-cop">
+        <img src={clientImage} />
+        <h4>VISTA CLIENTE </h4>
+      </div>
+      <div className="bottom-item item-cop">
+        <img src={discountImage} />
+        <h4>CÓDIGO PROMOCIONAL </h4>
+      </div>
+    </div>
+  );
 }
-
 export default Coupons;
